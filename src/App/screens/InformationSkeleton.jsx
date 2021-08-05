9d63834c7b5639226a0fbc53f20d2f5bfdbbbcc2
gn-grc <genejogarcia.gg@gmail.com>
2021-08-05 15:12:56 +0800

    added edit fab button and redirect

--- src/App/screens/InformationSkeleton.jsx
+++ src/App/screens/InformationSkeleton.jsx
@@ -1,16 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
+// Router
+import { Link } from "react-router-dom";
+
+// MUI
+import { makeStyles } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import Fab from "@material-ui/core/Fab";
+import EditIcon from "@material-ui/icons/Edit";
+
+// Styles
+const useStyles = makeStyles((theme) => ({
+  flexRoot: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+  },
+  title: {},
+  actions: {},
+  editIcon: {},
+}));
 
 function InformationSkeleton({ classes, id, title, rawKeywords, description }) {
+  const [hover, setHover] = useState(false);
+
+  // Styles, Additional Classes
+  const addClasses = useStyles();
+
   return (
     <Card className={classes.information}>
       <CardContent>
-        <Typography className={classes.title} variant="h6" color="primary">
-          {title}
-        </Typography>
+        <div className={addClasses.flexRoot}>
+          <div className={addClasses.title}>
+            <Typography className={classes.title} variant="h6" color="primary">
+              {title}
+            </Typography>
+          </div>
+
+          <div className={addClasses.actions}>
+            <Fab
+              onMouseEnter={() => {
+                setHover(true);
+              }}
+              onMouseLeave={() => {
+                setHover(false);
+              }}
+              component={Link}
+              to={`/edit/${id}/title/${title}/`}
+              size="small"
+              color={hover ? "primary" : "default"}
+            >
+              <EditIcon size="small" className={addClasses.editIcon} />
+            </Fab>
+          </div>
+        </div>
 
         <Typography className={classes.keywords} variant="subtitle2">
           {rawKeywords.map((item, index) => {
